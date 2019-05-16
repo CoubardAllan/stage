9f87f16a10a8143ced5128a9aeedab92e5e39ac3
allan coubard <allancoubard@gmail.com>
2019-05-16 09:25:45 +0200

    debut du mini projet 1 jour4

---
+++ projet1/src/ajoutformulaire.php
@@ -0,0 +1,25 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<!doctype HTML>
+<html>
+<head>
+    <title>formulaire</title>
+</head>
+
+<body>
+
+<H1><center> Ajout d'un article </center></H1><br><br>
+
+<a href="/projet1/interface.html">>Retour<</a><h4> <br><br><br><br>
+
+    <form method="post">
+        <label>nom Personnage</label><input type="varchar" name="Titre"><br>
+        <label>Prenom Personnage</label> <input type="datetime" name="Date"><br>
+        <label>Mot de Passe</label> <input type="varchar" name="Auteur">
+        <br><br>
+        <label>Email</label> <input type="longtext" name="Contenu" style="width:950px";" >
+        <br><br><br>
+        <input type="submit" value="Ajouter">
+    </form>
+</body>
+</html>
