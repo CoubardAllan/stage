3d12f583474a404d9cef9d9da328eb0a27eb771b
allan coubard <allancoubard@gmail.com>
2019-05-20 09:03:51 +0200

    derniere modif de la premiere semaine

--- MPjour5/ajoutformulaire.php
+++ MPjour5/ajoutformulaire.php
@@ -1,4 +1,36 @@
+<?php
+require_once 'src/personnage.php';
+require_once 'src/connexion.php';
+require_once 'src/requete.php';
+class ajout {
+    public function __construct()
+    {
 
+        try {
+
+            $dbh = new PDO('mysql:host=localhost;dbname=mp1', 'root', '');
+
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $nomrecup = $_POST['nom'];
+                $prenomrecup = $_POST['prenom'];
+                $pseudoRecup = $_POST['pseudo'];
+                $mdpRecup = $_POST['mdp'];
+                $emailRecup = $_POST['email'];
+
+                $pdst = "INSERT INTO personnage(id, nom, prenom, pseudo, mdp, email ) 
+            VALUES (NULL ,'$nomrecup','$prenomrecup','$pseudoRecup',' $mdpRecup','$emailRecup')";
+
+            $dbh->exec($pdst);
+            echo "L'ajout a été effectuer";
+            header('Location: interface.php');
+            exit();
+
+        } catch (PDOException $e) {
+            echo $pdst . "<br>" . $e->getMessage();
+        }
+    }
+}
+?>
 <!doctype HTML>
 <html>
 <head>
@@ -13,13 +45,13 @@
 
 <a href="interface.php">>Retour<</a><h4> <br><br><br><br>
 
-    <form method="post" action="src/ajout.php">
-        <label>nom Personnage</label><input type="varchar" name="Titre"><br>
-        <label>Prenom Personnage</label> <input type="datetime" name="Date"><br>
-        <label>Pseudo</label><input type="varchar" name="Titre"><br>
-        <label>Mot de Passe</label> <input type="varchar" name="Auteur">
+    <form method="post">
+        <label>nom Personnage</label><input type="text" name="nom"><br>
+        <label>Prenom Personnage</label> <input type="text" name="prenom"><br>
+        <label>Pseudo</label><input type="text" name="pseudo"><br>
+        <label>Mot de Passe</label> <input type="password" name="mdp">
         <br><br>
-        <label>Email</label> <input type="longtext" name="Contenu" style="width:950px";" >
+        <label>Email</label> <input type="longtext" name="email">
         <br><br><br>
         <input type="submit" value="Ajouter">
     </form>
