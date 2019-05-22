0b426d9ecc97fe857e1515c21b792c62ef0de699
allan coubard <allancoubard@gmail.com>
2019-05-22 14:15:34 +0200

    debut modif singleton session jour8 14h16

--- src/session.php
+++ src/session.php
@@ -3,23 +3,24 @@ require_once 'src/connexion.php';
 require_once 'src/articles.php';
 require_once 'src/requete.php';
 class session{
-    private $session;
-    public function __construct(){
+    private static $session;
+    private function __construct()
+    {
         if (session_status() !== PHP_SESSION_ACTIVE) {
             session_start();
+            self::$session = new session();
         }
     }
 
-
-    public function get($key)
+    public static function get($key)
     {
         return $_SESSION[$key];
     }
-    public function set($key, $values)
+    public static function set($key, $values)
     {
         $_SESSION[$key]=  $values;
     }
-    public function existe($key){
+    public static function existe($key){
         if ((!empty($_SESSION[$key])) && (isset($_SESSION[$key]))  ){
             return true;
         }else{
