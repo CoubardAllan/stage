36232910b7860536a50f001f006955cad59e3939
allan coubard <allancoubard@gmail.com>
2019-05-23 16:24:00 +0200

    modif jour9 16h25 derniere modif login

--- src/login.php
+++ src/login.php
@@ -8,12 +8,11 @@ class login{
     }
     public function identification(){
         if((isset($_POST['compte']) && (isset($_POST['mdp']))) && ((!empty($_POST['compte'])) && (!empty($_POST['mdp']))) ){
-            $query = "SELECT * FROM utilisateur WHERE nom_compte = ? AND mot_de_passe = ?";
+            $query = "SELECT nom_compte, mot_de_passe FROM utilisateur WHERE nom_compte = ? AND mot_de_passe = ?";
             $result = $this->login->prepare($query);
             $result->execute([$_POST['compte'],$_POST['mdp']]);
             $requete2 = $result->fetchAll();
             var_dump($requete2);
-
         }elseif((!isset($_POST['compte']) || (!isset($_POST['mdp']))) || ((empty($_POST['compte'])) || (empty($_POST['mdp']))) ){
             $message = 'champ necessaire';
             echo '<strong>'.$message.'</strong>';
